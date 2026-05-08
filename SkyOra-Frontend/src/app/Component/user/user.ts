d41edf225e58f31da674e611003bcd2c35510ff1
Prashast <prashast3617@gmail.com>
2026-05-08 17:05:18 +0530

    chore: resolve merge conflicts after stash pop

--- SkyOra-Frontend/src/app/Component/user/user.ts
+++ SkyOra-Frontend/src/app/Component/user/user.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../services/user';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class UserComponent implements OnInit {
+  users: any[] = [];
+
+  constructor(private userService: User, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        console.log('Users fetched successfully:', this.users);
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      }
+    });
+  }
+}
--- SkyOra-Frontend/src/app/app.ts
+++ SkyOra-Frontend/src/app/app.ts
@@ -6,7 +6,7 @@ import { Navbar } from "./Component/navbar/navbar";
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('SkyOra-Frontend');
